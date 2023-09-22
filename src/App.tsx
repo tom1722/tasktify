27b5c27d477e534740930a84b2f5c50fd58c9ed9
Tom Kalaw <kalawtomlorenz@gmail.com>
2023-09-22 02:10:01 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
+import TodoList from './components/TodoList';
+
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setcompletedTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo , isDone:false}]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
+  return <div className="App">
+    <span className="heading">Tasktify</span>
+    <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+    <TodoList todos={todos} setTodos={setTodos} />
+    
+  </div>;
+}
+
+export default App;
+
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./styles.css";
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+
+interface Props{
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({todos, setTodos}: Props) => {
+  return (
+    <div className='container'>
+      <div className="todos">
+        <span className="todos_heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos remove">
+      <span className="todos_heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+    </div>
+  )
+
+};
+
+export default TodoList
